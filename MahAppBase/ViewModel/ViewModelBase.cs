703b986e1884c7f378b507dcdd181ad3a4987f4d
Leo <leoyang@jepun.com.tw>
2019-10-23 17:45:15 +0800

    first version commit

---
+++ MahAppBase/ViewModel/ViewModelBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MahAppBase.ViewModel
+{
+    /// <summary>
+    /// 基礎ViewModel，繼承、實作INotifyPropertyChanged Interface
+    /// </summary>
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        #region Declarations
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region MemberFunction
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
